ea96a95ee7d57323e55b618c32c668291b4f45c5
guilhermeev6 <guilhermeevts@gmail.com>
2024-01-20 16:51:55 -0300

    Aula 63: Envio de Form

--- 5_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -6,25 +6,37 @@ const MyForm = () => {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
 
+  
   //Esse evento vai puxar todas as informações da ação do input
-  const handleName = (evento) => {
+  const handleName = (event) => {
     // Para cada mudança, a variável name vai ser alterada atravéz da função setName.
-    setName(evento.target.value)
+    setName(event.target.value)
+    // Se for apenas para salvar um valor, é melhor fazer inline assim como no email.
   }
-  console.log(name)
+  
+  const handleSubmit = (e) => {
+    // Vai parar o recarregamento da página para melhor experiência do usuário
+    e.preventDefault()
+    console.log("Enviando o formulário...")
+    console.log(name, email)
+  }
 
   return (
     <>
-      {/* criando oformulário */}
-      <form>
+      {/* criando o formulário */}
+      {/*5 - Envio de Formulário */}
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Nome: </label>
-          <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
+          <label>
+            <span>Nome: </span>
+            <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
+          </label>
         </div>
         {/* 2 - Label envolvendo input */}
         <label>
           <span>E-mail: </span>
-          <input type="email" name="email" placeholder="Digite seu E-mail" />
+          {/* 4 - Simplificação de manipulação de state */}
+          <input type="email" name="email" placeholder="Digite seu E-mail" onChange={(event) => setEmail(event.target.value)} />
         </label>
         <input type="submit" value="Enviar" />
       </form>
